cb3410de116b0ab5dce877b2278650a4e105955b
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-03-08 10:34:01 -0800

    turn bot back on

--- src/helpers/handleNotification.ts
+++ src/helpers/handleNotification.ts
@@ -49,68 +49,68 @@ export default async function (notification: Notification) {
     await SeenCastModel.create({
       hash: notification.content.cast.hash,
     })
-    // // Get response
-    // const conversation = await ConversationModel.findOne({
-    //   threadHash,
-    // })
-    // let { text: response, id: messageId } = await chatgpt.sendMessage(
-    //   `Write a knowledgeable reply to the following message: "${notification.content.cast.text}". Keep the reply shorter than 320 characters. Do not use hashtags.`,
-    //   {
-    //     parentMessageId: conversation?.currentParentMessageId,
-    //   }
-    // )
-    // let numberOfTries = 0
-    // while ((!response || response.length > 320) && numberOfTries < 10) {
-    //   if (numberOfTries > 0) {
-    //     console.log('======')
-    //     console.log(response.length, response)
-    //     console.log(
-    //       `Try #${numberOfTries + 1} for "${notification.content.cast.text}"`
-    //     )
-    //     await delay(15)
-    //   }
-    //   numberOfTries++
-    //   const newResponse = await chatgpt.sendMessage(
-    //     `Try again but. Keep reply under 320 characters and without hashtags.`,
-    //     {
-    //       parentMessageId: conversation?.currentParentMessageId,
-    //     }
-    //   )
-    //   response = newResponse.text
-    //   messageId = newResponse.id
-    // }
-    // console.log(response, messageId)
-    // await ConversationModel.updateOne(
-    //   {
-    //     threadHash,
-    //   },
-    //   {
-    //     currentParentMessageId: messageId,
-    //   },
-    //   {
-    //     upsert: true,
-    //   }
-    // )
-    // console.log('======')
-    // console.log('threadHash', threadHash)
-    // console.log(notification.content.cast.text)
-    // console.log(response.length, response)
+    // Get response
+    const conversation = await ConversationModel.findOne({
+      threadHash,
+    })
+    let { text: response, id: messageId } = await chatgpt.sendMessage(
+      `Write a knowledgeable reply to the following message: "${notification.content.cast.text}". Keep the reply shorter than 320 characters. Do not use hashtags.`,
+      {
+        parentMessageId: conversation?.currentParentMessageId,
+      }
+    )
+    let numberOfTries = 0
+    while ((!response || response.length > 320) && numberOfTries < 10) {
+      if (numberOfTries > 0) {
+        console.log('======')
+        console.log(response.length, response)
+        console.log(
+          `Try #${numberOfTries + 1} for "${notification.content.cast.text}"`
+        )
+        await delay(15)
+      }
+      numberOfTries++
+      const newResponse = await chatgpt.sendMessage(
+        `Try again but. Keep reply under 320 characters and without hashtags.`,
+        {
+          parentMessageId: conversation?.currentParentMessageId,
+        }
+      )
+      response = newResponse.text
+      messageId = newResponse.id
+    }
+    console.log(response, messageId)
+    await ConversationModel.updateOne(
+      {
+        threadHash,
+      },
+      {
+        currentParentMessageId: messageId,
+      },
+      {
+        upsert: true,
+      }
+    )
+    console.log('======')
+    console.log('threadHash', threadHash)
+    console.log(notification.content.cast.text)
+    console.log(response.length, response)
 
-    // response = response.trim()
-    // if (response.startsWith('"')) {
-    //   response = response.substring(1)
-    // }
-    // if (response.endsWith('"')) {
-    //   response = response.substring(0, response.length - 1)
-    // }
-    // if (response.length <= 320) {
-    //   return publishCast(response, notification.content.cast.hash)
-    // } else {
-    //   return publishCast(
-    //     'I tried 10 times to generate a reply under 320 characters but failed. So sorry, try again later!',
-    //     castHash
-    //   )
-    // }
+    response = response.trim()
+    if (response.startsWith('"')) {
+      response = response.substring(1)
+    }
+    if (response.endsWith('"')) {
+      response = response.substring(0, response.length - 1)
+    }
+    if (response.length <= 320) {
+      return publishCast(response, notification.content.cast.hash)
+    } else {
+      return publishCast(
+        'I tried 10 times to generate a reply under 320 characters but failed. So sorry, try again later!',
+        castHash
+      )
+    }
   } catch (error) {
     console.log(error instanceof Error ? error.message : error)
     if (castHash) {
